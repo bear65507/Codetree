de285f39323b5e88aee9a8cb75938c963bb1c197
Buttergom_03 <79465105+bear65507@users.noreply.github.com>
2025-03-27 22:35:36 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250327/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.cpp
+++ 250327/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.cpp
@@ -5,6 +5,8 @@ using namespace std;
 string text;
 string pattern;
 
+int ans;
+
 bool IsSame(int tidx)
 {
     int pidx = 0;
@@ -20,7 +22,23 @@ bool IsSame(int tidx)
     return true;
 }
 
-int FindIndex()
+bool ReverseIsSame(int tidx)
+{
+    int endtdix = text.length() - 1;
+    int pidx = pattern.length() - 1;
+
+    for (int i = tidx; i >= 0; i--) 
+    {
+        if (pidx < 0)
+            return true;
+        if (text[i] != pattern[pidx])
+            return false;
+        pidx--;
+    }
+    return true;
+}
+
+bool FindIndex()
 {
     for (int i = 0; i < text.length(); i++)
     {
@@ -28,17 +46,41 @@ int FindIndex()
         {
             if (text[i] == pattern[j])
                 if (IsSame(i))
-                    return i;
+                {
+                    ans = i;
+                    return true;
+                }
         }
     }
-    return -1;
+    return false;
 }
 
+bool ReverseFindIndex()
+{
+    for (int i = text.length() - 1; i >= 0; i--)
+    {
+        for (int j = pattern.length() - 1; j >= 0; j--)
+        {
+            if (text[i] == pattern[j])
+                if (ReverseIsSame(i))
+                    return true;
+        }
+    }
+    return false;
+}
+
+int IsAnswer()
+{
+    if (FindIndex() and ReverseFindIndex())
+        return ans;
+    else
+        return -1;
+}
 int main() {
     cin >> text;
     cin >> pattern;
 
-    cout << FindIndex() << endl;
+    cout << IsAnswer() << endl;
 
     return 0;
 }
