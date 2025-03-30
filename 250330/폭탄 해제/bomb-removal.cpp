bafa8ecd6862c95da52d987cbcf08c8b68e9bcac
Buttergom_03 <79465105+bear65507@users.noreply.github.com>
2025-03-30 14:37:28 +0900

    Title: 폭탄 해제, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250330/폭탄 해제/bomb-removal.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string unlock_code;
+char wire_color;
+int seconds;
+
+class Deactivation
+{
+public:
+    Deactivation(string code, char color, int sec)
+    {
+        this->_code = code;
+        this->_color = color;
+        this->_seconds = sec;
+    }
+    void PrintInfo()
+    {
+        cout << "code : " << _code << endl;
+        cout << "color : " << _color << endl;
+        cout << "second : " << _seconds << endl;
+    }
+public:
+    string _code;
+    char _color;
+    int _seconds;
+};
+
+int main() {
+    cin >> unlock_code >> wire_color >> seconds;
+
+    Deactivation da = Deactivation(unlock_code, wire_color, seconds);
+    da.PrintInfo();
+
+    return 0;
+}
