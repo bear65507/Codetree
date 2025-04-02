62cc1871cb68bb2294073f54553f596762d105eb
Buttergom_03 <79465105+bear65507@users.noreply.github.com>
2025-04-02 19:41:05 +0900

    Title: 원점으로부터의 거리, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250402/원점으로부터의 거리/distance-from-origin.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N;
+int x[1000];
+int y[1000];
+
+enum
+{
+    MAX_N = 1000
+};
+
+class Pos
+{
+public:
+    Pos()
+    {
+
+    }
+    Pos(int x, int y, int number)
+    {
+        this->_x = x;
+        this->_y = y;
+        this->_number = number;
+    }
+
+    void GetManhattanDis()
+    {
+        int posX, posY;
+        if (_x < 0)
+            posX = -_x;
+        else
+            posX = _x;
+        if (_y < 0)
+            posY = -_y;
+        else
+            posY = _y;
+        _distance = posX + posY;
+    }
+
+public:
+    int _x;
+    int _y;
+    int _distance = -1;
+    int _number;
+};
+
+bool Compare(Pos p1, Pos p2)
+{
+    return p1._distance < p2._distance;
+}
+
+void PrintInfo(Pos p[])
+{
+    for (int i = 0; i < N; i++)
+    {
+        cout << p[i]._number << endl;
+    }
+}
+
+int main() {
+    cin >> N;
+
+    Pos* p = new Pos[MAX_N];
+    for (int i = 0; i < N; i++) {
+        cin >> x[i] >> y[i];
+
+        p[i] = Pos(x[i], y[i], (i + 1));
+        p[i].GetManhattanDis();
+    }
+
+    sort(p, p + N, Compare);
+
+    PrintInfo(p);
+
+    delete[] p;
+    return 0;
+}
