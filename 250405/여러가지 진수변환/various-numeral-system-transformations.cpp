c1ac2326d062fabdbed68b87ee3495e7b843b65f
Buttergom_03 <79465105+bear65507@users.noreply.github.com>
2025-04-05 15:56:25 +0900

    Title: 여러가지 진수변환, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250405/여러가지 진수변환/various-numeral-system-transformations.cpp
+++ 250405/여러가지 진수변환/various-numeral-system-transformations.cpp
@@ -4,60 +4,30 @@ using namespace std;
 
 int N, B;
 
-void ToOct(int N)
+void Numeral(int N)
 {
-    int oct[20] = {};
-    int idx_o = 0;
+    int numeral[20] = {};
+    int idx = 0;
 
     while (true)
     {
-        if (N < 8)
+        if (N < B)
         {
-            oct[idx_o++] = N;
+            numeral[idx++] = N;
             break;
         }
-        oct[idx_o++] = N % 8;
-        N /= 8;
+        numeral[idx++] = N % B;
+        N /= B;
     }
 
-    for (int i = idx_o - 1; i >= 0; i--)
-        cout << oct[i];
+    for (int i = idx - 1; i >= 0; i--)
+        cout << numeral[i];
 }
-void ToFour(int N)
-{
-    int four[20] = {};
-    int idx_f = 0;
-
-    while (true)
-    {
-        if (N < 4)
-        {
-            four[idx_f++] = N;
-            break;
-        }
-        four[idx_f++] = N % 4;
-        N /= 4;
-    }
-
-    for (int i = idx_f - 1; i >= 0; i--)
-        cout << four[i];
-}
-
 
 int main() {
     cin >> N >> B;
     
-    switch (B)
-    {
-    case 4:
-        ToFour(N);
-        break;
-    case 8:
-        ToOct(N);
-        break;
-    default:
-        break;
-    }
+    Numeral(N);
 
     return 0;
 }
