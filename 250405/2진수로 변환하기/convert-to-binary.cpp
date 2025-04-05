b5ccf0cc0a068f5be0c5edbc804bd6edf194cd9e
Buttergom_03 <79465105+bear65507@users.noreply.github.com>
2025-04-05 15:29:34 +0900

    Title: 2진수로 변환하기, Time: 5ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250405/2진수로 변환하기/convert-to-binary.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int n;
+
+int main() {
+    cin >> n;
+    int digits[20] = {};
+    int idx = 0;
+    
+    while (true)
+    {
+        if (n < 2)
+        {
+            digits[idx++] = n;
+            break;
+        }
+
+        digits[idx++] = n % 2;
+        n /= 2;
+    }
+
+    for (int i = idx - 1; i >= 0; i--)
+        cout << digits[i];
+
+    return 0;
+}
