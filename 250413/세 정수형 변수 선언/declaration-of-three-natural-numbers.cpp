e887b5e27f666fcc51a80e0ef4bc82bcc060b068
Buttergom_03 <79465105+bear65507@users.noreply.github.com>
2025-04-13 14:09:45 +0900

    Title: 세 정수형 변수 선언, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250413/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x1 = 7, x2 = 23;
+    int res = x1 + x2;
+
+    cout << x1 << " + " << x2 << " = " << res << endl;
+    return 0;
+}
