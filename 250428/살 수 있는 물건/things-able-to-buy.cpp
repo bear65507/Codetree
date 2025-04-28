9fd1d69cb8dda2d19ee1f1e2e610928548d48266
Buttergom_03 <79465105+bear65507@users.noreply.github.com>
2025-04-28 13:13:23 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250428/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int money;
+    cin >> money;
+
+    if (money >= 3000)
+        cout << "book" << endl;
+    else if (money >= 1000)
+        cout << "mask" << endl;
+    else
+        cout << "no" << endl;
+        
+    return 0;
+}
