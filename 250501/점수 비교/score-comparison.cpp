7a21f642b2dff056a6f121928b5420e35028f4bc
Buttergom_03 <79465105+bear65507@users.noreply.github.com>
2025-05-01 14:18:45 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250501/점수 비교/score-comparison.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a_math, a_eng, b_math, b_eng;
+    cin >> a_math >> a_eng;
+    cin >> b_math >> b_eng;
+
+    if (a_math > b_math and a_eng > b_eng)
+        cout << "1" << endl;
+    else
+        cout << "0" << endl;
+        
+    return 0;
+}
