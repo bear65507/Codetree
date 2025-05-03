f78c829f35a6be2715ca6a5d4d1ec55502e7f0ee
Buttergom_03 <79465105+bear65507@users.noreply.github.com>
2025-05-03 14:50:48 +0900

    Title: 중앙값 구하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250503/중앙값 구하기/find-the-median.cpp
+++ 250503/중앙값 구하기/find-the-median.cpp
@@ -1,35 +1,29 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-void BobbleSort(int arr[])
-{
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2 - i; j++)
-        {
-            if (arr[i] > arr[i + 1])
-            {
-                int temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-            }
-        }
-    }
-}
 
 int main() {
     int n;
-    int arr[3];
+    vector<int> v;
     for (int i = 0; i < 3; i++)
     {
         cin >> n;
-        arr[i] = n;
+        v.push_back(n);
     }
 
-    BobbleSort(arr);
-    int median = arr[1];
+    int max = (v[0] > v[1] ? v[0] : v[1]);
+    max = (max > v[2] ? max : v[2]);
+    int min = (v[0] < v[1] ? v[0] : v[1]);
+    min = (min < v[2] ? min : v[2]);
+
+    int median;
+    for (int i = 0; i < 3; i++)
+    {
+        if (v[i] > min and v[i] < max)
+            median = v[i];
+    }
 
     cout << median << endl;
-
     return 0;
 }
