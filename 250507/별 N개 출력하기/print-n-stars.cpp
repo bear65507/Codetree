c26b38afb5bcafd0be1542ee041a977fc7cc4e4c
Buttergom_03 <79465105+bear65507@users.noreply.github.com>
2025-05-07 16:20:47 +0900

    Title: 별 N개 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250507/별 N개 출력하기/print-n-stars.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i = 0;
+    cin >> n;
+
+    while(i < n)
+    {
+        cout << "*" << endl;
+        i++;
+    }
+
+    return 0;
+}
