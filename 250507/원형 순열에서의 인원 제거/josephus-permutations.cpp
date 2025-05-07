ae5e6d28e359a8a9dd24873279dada535faf89c8
Buttergom_03 <79465105+bear65507@users.noreply.github.com>
2025-05-07 15:02:09 +0900

    Title: 원형 순열에서의 인원 제거, Time: 17ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250507/원형 순열에서의 인원 제거/josephus-permutations.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int N, K;
+
+int main() {
+    cin >> N >> K;
+    queue<int> q;
+    for (int i = 1; i <= N; i++)
+        q.push(i);
+    
+    while (q.size() != 1)
+    {
+        for (int i = 1; i < K; i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << q.front() << endl;
+
+    return 0;
+}
