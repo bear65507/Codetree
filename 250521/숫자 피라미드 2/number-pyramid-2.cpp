08a35d9b4afc4a95cef76069a271b0c5808935ea
Buttergom_03 <79465105+bear65507@users.noreply.github.com>
2025-05-21 14:55:48 +0900

    Title: 숫자 피라미드 2, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250521/숫자 피라미드 2/number-pyramid-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, cnt = 1;
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << cnt << " ";
+            cnt++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
