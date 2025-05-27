78fc3329a2e2b3b5100e5d90ab9cc29fb4b4dc2e
Buttergom_03 <79465105+bear65507@users.noreply.github.com>
2025-05-27 19:06:48 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250527/연속부분수열일까/contiguous-array-or-not.cpp
+++ 250527/연속부분수열일까/contiguous-array-or-not.cpp
@@ -1,17 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n1, n2, a[100], b[100] = { -1 };
-    cin >> n1 >> n2;
-
-    for (int i = 0; i < n1; i++)
-        cin >> a[i];
-    for (int j = 0; j < n2; j++)
-        cin >> b[j];
-
-    int a_length = n1, b_length = n2;
-    bool isMCNSS = false;
+bool IsMCNSS(int a[], int b[], int a_length, int b_length)
+{
     for (int i = 0; i < a_length; i++)
     {
         if (a[i] == b[0] and (i + b_length <= a_length))
@@ -26,14 +17,27 @@ int main() {
             }
             if (cnt == b_length)
             {
-                isMCNSS = true;
-                break;
+                return true;
             }
             i += cnt;
         }
     }
+    return false;
+}
 
-    if (isMCNSS)
+int main() {
+    int n1, n2, a[100], b[100];
+    cin >> n1 >> n2;
+
+    for (int i = 0; i < n1; i++)
+        cin >> a[i];
+    for (int j = 0; j < n2; j++)
+        cin >> b[j];
+
+    int a_length = n1, b_length = n2;
+
+
+    if (IsMCNSS(a, b, a_length, b_length))
         cout << "Yes" << endl;
     else
         cout << "No" << endl;
